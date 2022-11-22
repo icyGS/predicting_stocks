4385ba2cbed5dd5f10522aabc470ebeb8c629875
icyGS <108628576+icyGS@users.noreply.github.com>
2022-11-22 10:55:36 -0800

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
 import streamlit as st
+import torch
+from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import os
+
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
 st.title("Predicting Stocks App")
 st.write("Welcome to predicting stocks!")
-st.text_input('Todays news: ')
+article = st.text_input('Todays news: ')
+
+
+tokenizer = AutoTokenizer.from_pretrained("icyGS/FinancePredictor")
+
+model = AutoModelForSequenceClassification.from_pretrained("icyGS/FinancePredictor")
+
+classifier = pipeline("text-classification", model = model, tokenizer = tokenizer)
+prediction = classifier(article)
+st.text(prediction)
